7cb41d5729c6f940c7d6d7b5b94657d69a5cede0
josuedekpe <josue_qp19@outlook.com>
2023-05-05 11:06:01 -0500

    hdhd

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,13 @@
-const Sequelize=require('sequelize')
+const Sequelize = require('sequelize');
 
-const sequelize =new Sequelize(
+const sequelize = new Sequelize(
     process.env.PG_DB,
     process.env.PG_USER,
     process.env.PG_PASSWORD,
     {
-        host:process.env.PG_HOST,
-        dialect:'postgres',
+        host: process.env.PG_HOST,
+        dialect: 'postgres',
     }
-
 );
 
-module.exports=sequelize;
+module.exports = sequelize;
